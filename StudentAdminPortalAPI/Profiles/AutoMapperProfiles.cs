26af4db7f1c28cb8fec638e3f6d25f68fc0fec98
Bulumko <bulumko@racvert.co.za>
2025-11-15 14:38:44 +0200

    downloaded and commiting code from projects

---
+++ StudentAdminPortalAPI/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using StudentAdminPortalAPI.DomainModels;
+using StudentAdminPortalAPI.Profiles.AfterMaps;
+using DataModels = StudentAdminPortalAPI.DataModels;
+
+namespace StudentAdminPortalAPI.Profiles
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<DataModels.Student, Student>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Gender, Gender>()
+                .ReverseMap();
+
+            CreateMap<DataModels.Address, Address>()
+                .ReverseMap();
+
+            CreateMap<UpdateStudentRequest, DataModels.Student>()
+                .AfterMap<UpdateStudentRequestAfterMap>();
+
+            CreateMap<AddStudentRequest, DataModels.Student>()
+                .AfterMap<AddStudentRequestAfterMap>();
+
+
+        }
+        
+    }
+}
